dca8a53f3b714a7c286ea7259a6f911566b97f35
Pankaj Raghav <p.raghav@samsung.com>
2023-12-18 15:28:15 +0100

    Modified the WAF calculation and changed the round_up functionality.

--- src/lib.rs
+++ src/lib.rs
@@ -2,33 +2,18 @@
 extern crate approx;
 
 fn round_up(len: i32, base: i32) -> i32 {
-    ((len + base) / base) * base
+    ((len + base - 1) / base) * base
 }
 
 fn round_down(len: i32, base: i32) -> i32 {
     (len / base) * base
 }
-// all based on 4k values
+
 pub fn calculate_waf(offset: i32, len: i32, iu: i32) -> f64 {
-    let mut io_len = len;
-    let mut extra_io: i32 = 0;
+    let io_end = round_up(offset + len, iu);
+    let io_start = round_down(offset, iu);
 
-    if (offset & (iu - 1)) != 0 {
-        extra_io += iu;
-        io_len -= (round_up(offset, iu)) - offset;
-    }
-
-    if io_len < 0 {
-        return extra_io as f64 / len as f64;
-    }
-
-    // End is not aligned
-    if ((offset + len) & (iu - 1)) != 0 {
-        extra_io += iu;
-        io_len -= offset + len - (round_down(offset + len, iu));
-    }
-
-    (io_len as f64 + extra_io as f64) / len as f64
+    (io_end - io_start) as f64 / len as f64
 }
 
 #[cfg(test)]
@@ -61,5 +46,12 @@ mod tests {
         let result = 1.0625;
         // offset 12k, length 256k
         assert_relative_eq!(calculate_waf(3, 64, 4), result, max_relative = 0.000001);
+        let result = 1.33;
+        // offset 32k, length 24k
+        assert_relative_eq!(calculate_waf(8, 6, 4), result, max_relative = 0.01);
+        let result = 1.5;
+        // offset 8k, length 32k
+        assert_relative_eq!(calculate_waf(2, 8, 4), result, max_relative = 0.000001);
+        let result = 1.5;
     }
 }
